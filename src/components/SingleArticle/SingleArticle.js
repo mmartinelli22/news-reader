69e25baa9d8ebb221b87e7360b8e9da3b44dadb0
Jordan Farelli <jordanfarelli@gmail.com>
2022-10-31 18:11:18 -0600

    drilled funcionality to display single article details

--- src/components/SingleArticle/SingleArticle.js
+++ src/components/SingleArticle/SingleArticle.js
@@ -1,24 +1,24 @@
 import "./SingleArticle.css";
 import { Link } from "react-router-dom";
 
-const SingleArticle = ({ section, title, byline }) => {
+const SingleArticle = ({ section, title, byline, displaySingleArticle }) => {
   return (
-    // <div className='single-article-container'>
-    //   <p className='section'>{section.toUpperCase()}</p>
-    //   <Link to={'/details'} style={{ textDecoration: "none" }}>
-    //   <h2 className='article-title'>{title}</h2>
-    //   </Link>
-    //   <p className='byline'>{byline}</p>
-    // </div>
     <div className="single-article-container">
-      <p className="section">US</p>
-      <Link to={"/details"} style={{ textDecoration: "none" }}>
-        <h2 className="article-title">The Haunted Past</h2>
+      <p className="section">{section.toUpperCase()}</p>
+      <Link to={"/details"} style={{ textDecoration: "none", color: "black" }}>
+        <h2 className="article-title" onClick={() => displaySingleArticle(title)} value={title}>{title}</h2>
       </Link>
-      <p className="byline">
-        What ghost stories of the formerly enslaved tell us about their lives.
-      </p>
+      <p className="byline">{byline}</p>
     </div>
+    // <div className="single-article-container">
+    //   <p className="section">US</p>
+    //   <Link to={"/details"} style={{ textDecoration: "none" }}>
+    //     <h2 className="article-title">The Haunted Past</h2>
+    //   </Link>
+    //   <p className="byline">
+    //     What ghost stories of the formerly enslaved tell us about their lives.
+    //   </p>
+    // </div>
   );
 };
 
